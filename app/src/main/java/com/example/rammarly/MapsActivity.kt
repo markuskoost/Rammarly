c72a69eb3b12a3c4a28ec185d3375c82e672f69d
markus.koost <markuskoost@gmail.com>
2019-12-29 19:34:21 +0200

    Changed camera coordinates and zoom

--- app/src/main/java/com/example/rammarly/MapsActivity.kt
+++ app/src/main/java/com/example/rammarly/MapsActivity.kt
@@ -35,9 +35,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val location = LatLng(58.417, 22.500)
+        val locations = CameraUpdateFactory.newLatLngZoom(location, 8F)
+        mMap.animateCamera(locations)
     }
 }
