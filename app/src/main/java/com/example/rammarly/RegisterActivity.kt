4e7c45104f6f52a1a3c9cd1f1df7aa49a7f7caaf
Chimka <matflopie@gmail.com>
2020-01-04 03:34:52 +0200

    2 Password verification on register

--- app/src/main/java/com/example/rammarly/RegisterActivity.kt
+++ app/src/main/java/com/example/rammarly/RegisterActivity.kt
@@ -16,9 +16,11 @@ class RegisterActivity : AppCompatActivity() {
     //global variables
     private var email: String? = null
     private var password: String? = null
+    private var password2: String? = null
     //UI elements
     private var etEmail: EditText? = null
     private var etPassword: EditText? = null
+    private var etPassword2: EditText? = null
     private var btnCreateAccount: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +33,7 @@ class RegisterActivity : AppCompatActivity() {
     private fun initialise() {
         etEmail = findViewById<View>(R.id.et_email_re) as EditText
         etPassword = findViewById<View>(R.id.et_password_re) as EditText
+        etPassword2 = findViewById<View>(R.id.et_password_re2) as EditText
         btnCreateAccount = findViewById<View>(R.id.btn_register) as Button
         btnCreateAccount!!.setOnClickListener { createNewAccount() }
 
@@ -39,14 +42,17 @@ class RegisterActivity : AppCompatActivity() {
     private fun createNewAccount() {
         email = etEmail?.text.toString()
         password = etPassword?.text.toString()
-        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+        password2 = etPassword2?.text.toString()
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(password2))
+            if (password != password2){
+                Toast.makeText(this, "Passwordid ei ole samad!", Toast.LENGTH_SHORT).show()}
+            else {
             FirebaseAuth.getInstance()
                 .createUserWithEmailAndPassword(email!!, password!!)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
                         Log.d(TAG, "createUserWithEmail:success")
-                        val userId = FirebaseAuth.getInstance().currentUser!!.uid
                         //Verify Email
                         verifyEmail()
                         updateUserInfoAndUI()
